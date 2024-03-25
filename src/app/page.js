92c1ec821074c500d7e383067c060cc852ed0778
Robin Khuc <robin.khuc@beyondedge.co>
2024-03-25 23:17:18 +0700

    Fetch data from tmdb api in the homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <div>Home</div>;
+import Results from '@/components/Results';
+
+export const revalidate = 600;
+
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || 'fetchTrending';
+  const response = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  const results = data.results;
+  console.log({ results });
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
