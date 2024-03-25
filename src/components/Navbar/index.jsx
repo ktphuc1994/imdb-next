a5af66e574939ee163778e4db7d832a4a16241c4
Robin Khuc <robin.khuc@beyondedge.co>
2024-03-25 22:50:11 +0700

    create navbar component

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,10 @@
+import NavbarItem from './NavbarItem';
+
+export default function NavBar() {
+  return (
+    <div className='flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6'>
+      <NavbarItem title='Trending' param='fetchTrending' />
+      <NavbarItem title='Top Rated' param='fetchTopRated' />
+    </div>
+  );
+}
