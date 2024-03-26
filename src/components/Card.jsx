093b0c667301950b058bd475c1dcd19b33d0beef
Robin Khuc <robin.khuc@beyondedge.co>
2024-03-26 14:21:36 +0700

    add search box

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,10 +6,10 @@ import { FiThumbsUp } from 'react-icons/fi';
 export default function Card({ result }) {
   console.log(result.title, result.original_title, result.name);
   return (
-    <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
-      <Link href={`/movie/${result.id}`}>
+    <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 flex flex-col'>
+      <Link href={`/movie/${result.id}`} className='grow'>
         <Image
-          src={`https://image.tmdb.org/t/p/original${
+          src={`https://image.tmdb.org/t/p/original/${
             result.poster_path || result.backdrop_path
           }`}
           alt={result.title || result.name}
@@ -18,7 +18,7 @@ export default function Card({ result }) {
           className='sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300'
         />
       </Link>
-      <div className='p-2'>
+      <div className='p-2 shrink-0'>
         <p className='line-clamp-2 text-md'>{result.overview}</p>
         <h2 className='text-lg font-bold truncate'>
           {result.title || result.name}
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      aspectRatio: {
+        '5/3': '5 / 3',
+      },
     },
   },
   plugins: [],
