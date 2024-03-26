0b8f24c8c04a0134cd64321c028631a25f1e810d
Robin Khuc <robin.khuc@beyondedge.co>
2024-03-26 14:25:01 +0700

    fix the error.js file

--- src/app/error.js
+++ src/app/error.js
@@ -3,7 +3,10 @@
 import { useEffect } from 'react';
 
 export default function ErrorPage({ error, reset }) {
-  useEffect(() => {}, [error]);
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
   return (
     <div className='text-center mt-10'>
       <h1>Something went wrong. Please try again later.</h1>
